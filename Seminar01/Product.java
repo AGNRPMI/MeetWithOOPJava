b3e4593223b05fd07481f5c666f190cfc21775e5
AGNRPMI <pmi.petrov@yandex.ru>
2023-10-26 03:20:39 +0400

    new commit

--- Seminar01/Product.java
+++ Seminar01/Product.java
@@ -2,6 +2,46 @@ package Seminar01;
 
 import java.util.List;
 
+// реализация опций - просто класса, класса с инкапсуляцией и абстрактного класса
+
+
+// public class Product{
+//     public String name;
+//     public double cost;
+// }
+
+
+// public class Product{
+//     private String name;
+//     private double cost;
+
+
+//     public Product(String name, double cost) {
+//         this.name = name;
+//         this.cost = cost;
+//     }
+
+
+//     public String getName() {
+//         return name;
+//     }
+
+
+//     public double getCost() {
+//         return cost;
+//     }
+
+
+//     public void setName(String name) {
+//         this.name = name;
+//     }
+
+
+//     public void setCost(double cost) {
+//         this.cost = cost;
+//     }
+// }
+
 public abstract class Product {
     private String name;
     private double cost;
--- Seminar01/VendingMachine.java
+++ Seminar01/VendingMachine.java
@@ -3,8 +3,50 @@ package Seminar01;
 import java.util.ArrayList;
 import java.util.List;
 
-public interface VendingMachine {
-    Product getProduct(String name);
+//реализация двух опций - просто класса, инкапсуляции и интерфейса
 
-    Product getProduct(String name, double volume);
+// public class VendingMachine {
+
+//     List<Product> products = new ArrayList<>();
+
+//     public void initProduct (List<Product> products){
+//         this.products= products;
+//     }
+
+//     public Product getProduct(String name){
+//         for (Product item: products){
+//             if (item.name.equals(name)) return item;
+//         }
+//         return null;
+//     }    
+// }
+
+public class VendingMachine {
+    private List<Product> products = new ArrayList<>();
+
+    public VendingMachine(List<Product> products) {
+        this.products = products;
+    }
+
+    public Product getProduct(String name) {
+        for (Product item : products) {
+            if (item.getName().equals(name))
+                return item;
+        }
+        return null;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
 }
+// public interface VendingMachine {
+
+// Product getProduct(String name);
+
+// Product getProduct(String name, double volume);
+// }
